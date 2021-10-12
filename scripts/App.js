4cb92c62eb04b7baae4ae34f0d069216cd437ba4
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-12 20:10:45 +0200

    styles in react

--- scripts/App.js
+++ scripts/App.js
@@ -13,14 +13,24 @@ class App extends React.Component {
             shoppingCart: this.state.shoppingCart + 1,
         })
     }
+    handleBuy = () => {
+        this.setState({
+            availableProducts: this.state.availableProducts - this.state.shoppingCart,
+            shoppingCart: 0,
+        })
+    }
 
     render() {
+        const style = this.state.shoppingCart === 0 ? {opacity: .3} : {};
         return (
             <div>
                 <button disabled={this.state.shoppingCart === 0} onClick={this.handleRemoveFromCart}>-
                 </button>
-                <span>{this.state.shoppingCart}</span>
-                <button disabled={this.state.shoppingCart === this.state.availableProducts} onClick={this.handleAddToCart}>+</button>
+                <span style={style}>{this.state.shoppingCart}</span>
+                <button disabled={this.state.shoppingCart === this.state.availableProducts}
+                        onClick={this.handleAddToCart}>+
+                </button>
+                {this.state.shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
             </div>
         )
     }
